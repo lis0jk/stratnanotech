d7e0375b4f8be9f92cdd8e2f1ab218a24cce6b8a
lis0jk <lissjkk@gmail.com>
2023-08-01 22:54:22 +0300

    Created article page and fixed some bugs

--- app/js/main.js
+++ app/js/main.js
@@ -91,52 +91,72 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
   openImg();
+
+  function toggleSliderText() {
+    const checkbox = document.getElementById("read-more-checker");
+    const sliderUpIcon = document.querySelector(".slider-up");
+    const sliderDownIcon = document.querySelector(".slider-down");
+
+    checkbox.addEventListener("change", function () {
+      if (this.checked) {
+        sliderUpIcon.style.display = "block";
+        sliderDownIcon.style.display = "none";
+      } else {
+        sliderUpIcon.style.display = "none";
+        sliderDownIcon.style.display = "block";
+      }
+    });
+  }
+
+  toggleSliderText();
 });
 
-function init() {
-  var coords1 = [55.749633, 37.537434];
-  var coords2 = [53.90239, 27.639733];
+if (typeof ymaps !== "undefined") {
+  function init() {
+    var coords1 = [55.749633, 37.537434];
+    var coords2 = [53.90239, 27.639733];
 
-  // Создание карты
-  var map = new ymaps.Map("map", {
-    center: [54.826308, 35.89043], // Центр карты (может быть любая другая точка по вашему выбору)
-    zoom: 5, // Масштаб карты (число от 0 до 19)
-  });
+    // Создание карты
+    var map = new ymaps.Map("map", {
+      center: [54.826308, 35.89043], // Центр карты (может быть любая другая точка по вашему выбору)
+      zoom: 5, // Масштаб карты (число от 0 до 19)
+    });
 
-  // Добавление метки для первого места
-  var placemark1 = new ymaps.Placemark(
-    coords1,
-    {},
-    {
-      iconLayout: "default#image",
-      iconImageHref: "/images/icon/map.svg",
-      iconImageSize: [42, 60],
-      iconImageOffset: [0, 0],
-    }
-  );
-  map.geoObjects.add(placemark1);
+    // Добавление метки для первого места
+    var placemark1 = new ymaps.Placemark(
+      coords1,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "/images/icon/map.svg",
+        iconImageSize: [42, 60],
+        iconImageOffset: [0, 0],
+      }
+    );
+    map.geoObjects.add(placemark1);
 
-  // Добавление метки для второго места
-  var placemark2 = new ymaps.Placemark(
-    coords2,
-    {},
-    {
-      iconLayout: "default#image",
-      iconImageHref: "/images/icon/map.svg",
-      iconImageSize: [42, 60],
-      iconImageOffset: [0, 0],
-    }
-  );
-  map.geoObjects.add(placemark2);
+    // Добавление метки для второго места
+    var placemark2 = new ymaps.Placemark(
+      coords2,
+      {},
+      {
+        iconLayout: "default#image",
+        iconImageHref: "/images/icon/map.svg",
+        iconImageSize: [42, 60],
+        iconImageOffset: [0, 0],
+      }
+    );
+    map.geoObjects.add(placemark2);
 
-  map.controls.remove("geolocationControl"); // удаляем геолокацию
-  map.controls.remove("searchControl"); // удаляем поиск
-  map.controls.remove("trafficControl"); // удаляем контроль трафика
-  map.controls.remove("typeSelector"); // удаляем тип
-  map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
-  // map.controls.remove("zoomControl"); // удаляем контрол зуммирования
-  map.controls.remove("rulerControl"); // удаляем контрол правил
-  map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
-  // map.geoObjects.add(placemark);
+    map.controls.remove("geolocationControl"); // удаляем геолокацию
+    map.controls.remove("searchControl"); // удаляем поиск
+    map.controls.remove("trafficControl"); // удаляем контроль трафика
+    map.controls.remove("typeSelector"); // удаляем тип
+    map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
+    // map.controls.remove("zoomControl"); // удаляем контрол зуммирования
+    map.controls.remove("rulerControl"); // удаляем контрол правил
+    map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
+    // map.geoObjects.add(placemark);
+  }
+  ymaps.ready(init);
 }
-ymaps.ready(init);
