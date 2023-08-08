19e7dd7c37381490ae17508eca39b0a7905ed384
lis0jk <lissjkk@gmail.com>
2023-08-08 14:49:14 +0300

    Edit some bugs

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   function openModal() {
     document.addEventListener("click", function (event) {
-      if (event.target.classList.contains("vacancy__button")) {
+      if (event.target.classList.contains("vacancy__item-title")) {
         const vacancyItem = event.target.closest(".vacancy__item");
-        const modal = vacancyItem.querySelector(".vacancy__modal");
+        const modal = vacancyItem.querySelector(".modal");
 
         if (modal) {
           modal.style.display = "block";
