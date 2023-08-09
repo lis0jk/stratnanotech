eeedf88cbd1b10831e441a148a5f210c34990e4e
lis0jk <lissjkk@gmail.com>
2023-08-09 14:02:13 +0300

    Edits resume

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   function openModal() {
     document.addEventListener("click", function (event) {
-      if (event.target.classList.contains("vacancy__item-title")) {
+      if (event.target.classList.contains("vacancy__item-down")) {
+        const overlay = document.querySelector(".modal__overlay");
         const vacancyItem = event.target.closest(".vacancy__item");
         const modal = vacancyItem.querySelector(".modal");
 
         if (modal) {
+          overlay.style.display = "block";
           modal.style.display = "block";
 
           const modalCloseBtn = modal.querySelector(".modal__close");
           if (modalCloseBtn) {
             modalCloseBtn.addEventListener("click", function () {
               modal.style.display = "none";
+              overlay.style.display = "none";
             });
           }
         }
