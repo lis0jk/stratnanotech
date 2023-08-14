47c6a8fa9f0200575ae5d9205a4cca7c09e6eeef
lis0jk <lissjkk@gmail.com>
2023-08-14 11:15:49 +0300

    Edits modal

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,30 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var vacancyItems = document.querySelectorAll(".vacancy__item");
-  var modals = document.querySelectorAll(".modal");
-  var vacancyContainers = document.querySelectorAll(".vacancy__slider-item");
+  const vacancyItems = document.querySelectorAll(".vacancy__item");
 
   vacancyItems.forEach(function (vacancyItem) {
     vacancyItem.addEventListener("click", function () {
-      var modalId = this.getAttribute("data-modal-id");
-      var modal = document.querySelector(`.modal[data-modal-id="${modalId}"]`);
+      const modalId = this.getAttribute("data-modal-id");
+      const modal = document.querySelector(
+        `.modal[data-modal-id="${modalId}"]`
+      );
       if (modal) {
         modal.style.display = "block";
       }
     });
   });
 
-  vacancyContainers.forEach(function (vacancyContainer) {
-    vacancyContainer.addEventListener("click", function (e) {
-      if (e.target === this) {
-        this.style.display = "none";
-      }
-    });
-  });
-
-  var modalCloses = document.querySelectorAll(".modal__close");
+  const modalCloses = document.querySelectorAll(".modal__close");
   modalCloses.forEach(function (modalClose) {
     modalClose.addEventListener("click", function () {
-      var modal = this.closest(".modal");
+      const modal = this.closest(".modal");
       if (modal) {
         modal.style.display = "none";
       }
